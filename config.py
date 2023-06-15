ac978d1e78b7695b6748a8a8be052119475104f9
jonnyboooy <103967803+jonnyboooy@users.noreply.github.com>
2023-06-15 17:33:55 +0300

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-admin_id = []
-token = '5870745919:AAHwW1FzfznrvQUhRzp6XS3-swfmhqoH4yI'
-# '5870745919:AAHwW1FzfznrvQUhRzp6XS3-swfmhqoH4yI' # ktinder_bot
-# '5291296989:AAFeXKW2HfhYI9dsctnoKXzxu7od9kdTIUA' # Bratek_bot
