2b8187c567904ef3c3bb14b608000a95491653fd
anonim <70073044+l1v0n1@users.noreply.github.com>
2025-03-23 17:51:13 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-admin_id = [780544490]
-token = '6596353396:AAFY7-SdzHb7432PZpn7S5P1XmQcWKHlhsY'
-# '5870745919:AAHwW1FzfznrvQUhRzp6XS3-swfmhqoH4yI' # ktinder_bot
-# '5291296989:AAFeXKW2HfhYI9dsctnoKXzxu7od9kdTIUA' # Bratek_bot
-# '5708793678:AAFnD92fcGj4B--5a_-SRGkYyVKUxKQDMUw' # token Chico 1
-# '6141236765:AAGgJEgGuTef9AkjqXLN254zjc4Qs5qpvwI' # token Chico 2
